677a2f13273a8672c081a286131a97d417dce6eb
koga1020 <koga.shozo1020@gmail.com>
2022-02-07 21:48:55 +0900

    init

---
+++ lib/ex_connpass.ex
@@ -0,0 +1,18 @@
+defmodule ExConnpass do
+  @moduledoc """
+  Documentation for `ExConnpass`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExConnpass.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_connpass_test.exs
@@ -0,0 +1,8 @@
+defmodule ExConnpassTest do
+  use ExUnit.Case
+  doctest ExConnpass
+
+  test "greets the world" do
+    assert ExConnpass.hello() == :world
+  end
+end
