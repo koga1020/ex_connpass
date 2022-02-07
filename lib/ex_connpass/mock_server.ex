b488e0b00a3b3ae973229c60526e271f084a9df5
koga1020 <koga.shozo1020@gmail.com>
2022-02-07 22:39:43 +0900

    update mock_server
    - add not found route
    - add event result

--- lib/ex_connpass/mock_server.ex
+++ lib/ex_connpass/mock_server.ex
@@ -9,10 +9,14 @@ defmodule ExConnpass.MockServer do
       results_returned: 1,
       results_available: 100,
       results_start: 1,
-      events: []
+      events: [%{title: "sample event", event_id: 1}]
     }
 
     conn
     |> Plug.Conn.send_resp(200, Jason.encode!(resp))
+  end
+
+  get "/not-found-route" do
+    Plug.Conn.send_resp(conn, 404, "")
   end
 end
