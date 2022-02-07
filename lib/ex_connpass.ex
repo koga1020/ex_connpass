bd49f4e261a276161e00174cebb77cce14487ac6
koga1020 <koga.shozo1020@gmail.com>
2022-02-07 22:17:58 +0900

    add ExConnpass module

--- lib/ex_connpass.ex
+++ lib/ex_connpass.ex
@@ -1,18 +1,48 @@
 defmodule ExConnpass do
-  @moduledoc """
-  Documentation for `ExConnpass`.
-  """
+  import Commandex
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  command do
+    param :query
 
-      iex> ExConnpass.hello()
-      :world
+    data :api_url
+    data :events
+    data :response
 
-  """
-  def hello do
-    :world
+    pipeline :build_url
+    pipeline :call_api
+    pipeline :decode
+  end
+
+  def build_url(command, %{query: query}, _data) do
+    base_url = Application.get_env(:ex_connpass, :api_base_url, "http://localhost:8081/")
+    query_string = URI.encode_query(query)
+
+    api_url =
+      %{URI.parse(base_url) | query: query_string}
+      |> URI.to_string()
+
+    put_data(command, :api_url, api_url)
+  end
+
+  def call_api(command, _params, %{api_url: api_url}) do
+    Logger.info("call #{api_url}")
+
+    api_url
+    |> HTTPoison.get()
+    |> then(fn
+      {:error, error} ->
+        put_error(command, :call_api, error) |> halt()
+
+      {:ok, response} ->
+        put_data(command, :response, response)
+    end)
+  end
+
+  def decode(command, _params, %{response: %HTTPoison.Response{body: body}}) do
+    %{"events" => events} = Jason.decode!(body)
+
+    put_data(command, :events, events)
   end
 end
